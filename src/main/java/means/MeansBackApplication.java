73b9538ea25aea7ecee37369e89e3896cf01fb21
goloro <goloro02@gmail.com>
2022-03-18 11:47:05 +0100

    first commit

---
+++ src/main/java/means/MeansBackApplication.java
@@ -0,0 +1,13 @@
+package means;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeansBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeansBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/means/MeansBackApplicationTests.java
@@ -0,0 +1,13 @@
+package means;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeansBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
